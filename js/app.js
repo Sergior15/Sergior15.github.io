6bc8f2fb631542f791b49ce4186e95837e259fa7
unknown <rsergio98@yahoo.com>
2019-07-25 01:08:50 -0400

    code was not working when committed changed js to its own file and added onclick for sections and body

---
+++ js/app.js
@@ -0,0 +1,26 @@
+document.getElementsByTagName('body')[0].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+document.getElementsByTagName('section')[0].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+document.getElementsByTagName('section')[1].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+document.getElementsByTagName('section')[2].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+document.getElementsByTagName('section')[3].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+document.getElementsByTagName('section')[4].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+document.getElementsByTagName('section')[5].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+document.getElementsByTagName('div')[2].onclick = function() {
+    this.style.backgroundColor = `black`;
+}
+
+
